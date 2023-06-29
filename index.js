8c933812278ea59ce295fdf3d8758bc775cd1976
TheCodeWhy <136911594+TheCodeWhy@users.noreply.github.com>
2023-06-29 21:28:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function main(){
+  const maze = document.getElementById("maze");
+  maze.addEventListener("click", function() {
+    window.location.href = "https://thecodewhy.github.io/Maze/";
+  });
+  
+}
+
+main()
