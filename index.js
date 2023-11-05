d28d5f724018fd37cbc83e87d35ab0b6459ad733
code why <136911594+TheCodeWhy@users.noreply.github.com>
2023-11-05 02:28:42 +0100

    icons click major

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function main(){
     window.location.href = "https://thecodewhy.github.io/Maze/";
   });
   
+  const mathclimb = document.getElementById("mathclimb");
+  mathclimb.addEventListener("click", function() {
+    window.location.href = "https://thecodewhy.github.io/MathClimb/";
+  });
 }
 
 main()
